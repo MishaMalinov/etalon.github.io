3916314730dd2a23bd1daca37d9d13acb1ef7404
MishaMalinov <mishokmalinok123@gmail.com>
2024-03-27 01:16:06 +0100

    main lenta

--- pages/home/home.php
+++ pages/home/home.php
@@ -16,11 +16,11 @@ $products = json_decode(file_get_contents('../../data/production.json'), false);
                 </div>
                 <div class="list">
                     <?php
-                        foreach ($products as $product) {
-                            $img = "../../data/".$product->img."/0.jpeg";
-                            $title = $product->shortTitle;
-                            $id = $product->id;
-                            echo "
+                    foreach ($products as $product) {
+                        $img = "../../data/" . $product->img . "/0.jpeg";
+                        $title = $product->shortTitle;
+                        $id = $product->id;
+                        echo "
                                     <a  href='../item/item.php?id=$id' >
                                         <div class='block'>
                                             
@@ -30,8 +30,33 @@ $products = json_decode(file_get_contents('../../data/production.json'), false);
                                         </div>
                                     </a>
                                     ";
-                        }
+                    }
                     ?>
+                </div>
+            </div>
+            <div class="services">
+                <div class="title">Послуги</div>
+                <div class="list">
+                    <div class="list-el">
+                        <img src="../../data/slusWork.png" alt="">
+                        <div class="label">Слюсарні роботи</div>
+                    </div>
+                    <div class="list-el">
+                        <img src="../../data/gnutMetalu.png" alt="">
+                        <div class="label">Гнуття металу</div>
+                    </div>
+                    <div class="list-el">
+                        <img src="../../data/zvarWork.png" alt="">
+                        <div class="label">Зварювальні роботи</div>
+                    </div>
+                    <div class="list-el">
+                        <img src="../../data/tokarWork.png" alt="">
+                        <div class="label">Токарні роботи</div>
+                    </div>
+                    <div class="list-el">
+                        <img src="../../data/frezWork.png" alt="">
+                        <div class="label">Фрезерні роботи</div>
+                    </div>
                 </div>
             </div>
         </div>
